8c2a754301b6183a2a0a1daadd731860b8935a48
Dani S. <daniela.schomaker@posteo.de>
2025-09-11 14:10:29 +0200

    feat: add input field for new comments (in progress)

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,4 +1,4 @@
-////// Book Gallery //////
+////// Book Gallery-Template //////
 
 function getBookGalleryTemplate(indexBooks) {
   return `
@@ -7,88 +7,55 @@ function getBookGalleryTemplate(indexBooks) {
         <h2 class="padding_x">${books[indexBooks].name}</h2>
       </header>
 
-      <img src="./assets/img/book-image.png" alt="Abbildung des Buches">
+      <section class="book_illustration"> 
+        <img src="./assets/img/book-image.png" alt="Abbildung des Buches">
+      </section>
+      
+      <section class="book_description">
+        <div class="book_price_and_likes">
+          <span class="book_price">${books[indexBooks].price} €</span>
+          <div class="book_likes">
+            <span>${books[indexBooks].likes}</span>
+            <button class="like_button" onclick="addLike()">
+              <img src="./assets/icons/heart-thin-icon.png" alt="Like" />
+            </button>
+          </div>
+        </div>
 
-        <section class="book_description">
-            <div class="book_price_and_likes">
-              <span class="book_price">${books[indexBooks].price} €</span>
-              <div class="book_likes">
-                <span>${books[indexBooks].likes}</span>
-                <button class="like_button">
-                  <img src="./assets/icons/heart-thin-icon.png" alt="Like" />
-                </button>
-              </div>
-            </div>
+        <div class="book_metadata">
+          <table>
+            <tr>
+              <td>Autor:</td>
+              <td>${books[indexBooks].author}</td>
+            </tr>
+            <tr>
+              <td>Erscheinungsjahr:</td>
+              <td>${books[indexBooks].publishedYear}</td>
+            </tr>
+            <tr>
+              <td>Genre:</td>
+              <td>${books[indexBooks].genre}</td>
+            </tr>
+          </table>
+        </div>
+      </section>
 
-            <div class="book_metadata">
-              <table>
-                  <tr>
-                    <td>Autor:</td>
-                    <td>${books[indexBooks].author}</td>
-                  </tr>
-                  <tr>
-                    <td>Erscheinungsjahr:</td>
-                    <td>${books[indexBooks].publishedYear}</td>
-                  </tr>
-                  <tr>
-                    <td>Genre:</td>
-                    <td>${books[indexBooks].genre}</td>
-                  </tr>
-              </table>
-            </div>
-        </section>
-
-        <section class="comments">
+      <section class="comments">
+        <div class="existing_comments">
           <h3>Kommentare:</h3>
           ${getBookComments(indexBooks)}
-              
-        </section>
+        </div>
+
+        <div class="comment_formular">
+          <input type="text" placeholder="Schreibe deinen Kommentar..."/>
+          <button class="send_button" onclick="addComment()">
+            <img src="./assets/icons/paper-plane-icon.png" alt="Button: Kommentar senden" />    
+          </button>
+        </div>
+
+      </section>
 
 
-      </article>
+    </article>
           `;
 }
-
-
-
-function getBookComments(indexBooks) {
-  let commentsRef = "";
-  for (let indexComments = 0; indexComments < books[indexBooks].comments.length; indexComments++) {
-    commentsRef += `<p>${books[indexBooks].comments[indexComments].name}: ${books[indexBooks].comments[indexComments].comment}</p>`;
-  }
-  return commentsRef;
-}
-
-// function getProductsCharacteristics(index) {
-//   let characteristics = "";
-
-//   for (let indexCharacteristics = 0; indexCharacteristics < products[index].Eigenschaften.length; indexCharacteristics++) {
-//     // characteristics += "<br>" + "- " + products[index].Eigenschaften[indexCharacteristics];
-//     characteristics += `<li>${products[index].Eigenschaften[indexCharacteristics]}</li>`;
-//   }
-
-//   return `
-//         ${products[index].Name}: ${products[index].Preis} €<br>
-//         Kategorie: ${products[index].Kategorie}<br>
-//         Eigenschaften: ${characteristics}<br><br>
-//       `;
-// }
-
-  // let commentsRef = "";
-  // for (let i = 0; i < books[i].comments.length; i++) {
-  //   commentsRef += `<p>${books[i].comments[j].name}: ${books[i].comments[j].comment}</p>`;
-  // }
-
-
-
-//   return `
-//     <button class="photo_preview" onclick="openLightbox(${i})">
-//       <img
-//         src="${PHOTOS[i]}"
-//         alt="${PHOTOS_ALT[i]}"
-//         class="photo_preview_img"
-//        />
-//     </button>`;
-
-// die Fotos werden als Buttons eingefügt damit man sie per Tab+Enter (ohne Maus) öffnen kann
-// }
